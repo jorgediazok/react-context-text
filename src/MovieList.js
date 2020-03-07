e8748e4ffa2800ae3ccae3452d3b14e5d2ca76e3
Jorge <jorgediazok@gmail.com>
2020-03-07 16:44:05 -0300

    first commit

---
+++ src/MovieList.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import Movie from './Movie';
+
+const MovieList = () => {
+  const [movies, setMovies] = useState([
+    {
+      name: 'Harry Potter',
+      price: 10,
+      id: 123
+    },
+    {
+      name: 'Game of Thrones',
+      price: 15,
+      id: 321
+    },
+    {
+      name: 'Gone with the wind',
+      price: 20,
+      id: 222
+    },
+    {
+      name: 'Shazam',
+      price: 44,
+      id: 133
+    },
+    {
+      name: 'God of War',
+      price: 14,
+      id: 333
+    },
+    {
+      name: 'Ghost',
+      price: 45,
+      id: 253
+    }
+  ]);
+  return (
+    <div>
+      {movies.map(movie => (
+        <li>{movie.name}</li>
+      ))}
+    </div>
+  );
+};
+
+export default MovieList;
