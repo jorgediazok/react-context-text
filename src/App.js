9f80b6402360fea139ba9dda76b10344952419b8
Jorge <jorgediazok@gmail.com>
2020-03-08 15:11:24 -0300

    nav agregada

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import MovieList from './MovieList';
+import Nav from './Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <MovieList />
     </div>
   );
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div className="nav">
+      <h3>Jorge-Dev</h3>
+      <p>List of Movies:</p>
+    </div>
+  );
+};
+
+export default Nav;
